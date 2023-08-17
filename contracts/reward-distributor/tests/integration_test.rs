233145f286d0265a314cc8aa7c67b256ace47f02
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-17 23:51:59 +0200

    refactor: Move robot to new package and change robot trait to struct

--- contracts/reward-distributor/tests/integration_test.rs
+++ contracts/reward-distributor/tests/integration_test.rs
@@ -1,73 +1,83 @@
-use std::str::FromStr;
+use common::get_test_runner;
+use cosmwasm_std::Uint128;
+use cw_it::test_tube::Account;
+use cw_it::traits::CwItRunner;
 
-use apollo_cw_asset::Asset;
-use common::{get_test_runner, DefaultRewardDistributorRobot};
-use cosmwasm_std::{Coins, Uint128};
-use cw_dex::traits::Pool;
-use cw_it::astroport::astroport::asset::Asset as AstroAsset;
-use cw_it::{astroport::robot::AstroportTestRobot, test_tube::Account};
+use locked_astroport_vault_test_helpers::cw_vault_standard_test_helpers::traits::CwVaultStandardRobot;
+use locked_astroport_vault_test_helpers::robot::LockedAstroportVaultRobot;
+use neutron_astroport_reward_distributor_test_helpers as test_helpers;
 
-use crate::common::{RewardDistributorRobot, DEFAULT_LIQ};
+use test_helpers::robot::RewardDistributorRobot;
+
+use crate::common::{DEPS_PATH, UNOPTIMIZED_PATH};
 
 mod common;
 
 #[test]
 fn test_initialization() {
     let runner = get_test_runner();
-    let admin = DefaultRewardDistributorRobot::default_account(&runner);
-    let robot = DefaultRewardDistributorRobot::instantiate(&runner, &admin, 1000000u128);
+    let admin = RewardDistributorRobot::default_account(&runner);
+    let treasury_addr = runner.init_account(&[]).unwrap();
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let robot = RewardDistributorRobot::instantiate(
+        &runner,
+        &dependencies,
+        DEPS_PATH,
+        UNOPTIMIZED_PATH,
+        treasury_addr.address(),
+        &admin,
+        1000000u128,
+    );
 
-    // Query config
-    let config = robot.query_config();
+    // Query contract state
+    let state = robot.query_state();
+    let config = state.config;
     assert_eq!(config.emission_per_second, Uint128::from(1000000u128));
     assert_eq!(config.distribution_addr, robot.distribution_acc.address());
+    assert_eq!(state.reward_pool, robot.reward_pool);
     assert_eq!(
-        config.reward_lp_token.to_string(),
-        robot.reward_vault_robot.base_token
+        state.reward_pool.lp_token_addr.to_string(),
+        robot.reward_vault_robot.base_token()
     );
+    assert_eq!(state.reward_vault.addr, robot.reward_vault_robot.vault_addr);
     assert_eq!(
-        config.reward_vault_addr,
-        robot.reward_vault_robot.vault_addr
+        state.reward_vault.vault_token,
+        robot.reward_vault_robot.vault_token()
     );
-    assert_eq!(config.reward_vt_denom, robot.reward_vault_robot.vault_token);
-    assert_eq!(
-        config.reward_pool.lp_token().to_string(),
-        robot.reward_lp_addr
-    )
 }
 
 #[test]
 fn test_distribute() {
     let runner = get_test_runner();
-    let admin = DefaultRewardDistributorRobot::default_account(&runner);
-    let robot = DefaultRewardDistributorRobot::instantiate(&runner, &admin, 1000000u128);
-
-    // Provide liquidity to reward pool
-    let assets: Vec<AstroAsset> = Coins::from_str(DEFAULT_LIQ)
-        .unwrap()
-        .into_vec()
-        .into_iter()
-        .map(|x| Asset::from(x).into())
-        .collect();
-    robot.provide_liquidity(&robot.reward_pool_addr, assets, &admin);
+    let admin = RewardDistributorRobot::default_account(&runner);
+    let treasury_addr = runner.init_account(&[]).unwrap();
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let robot = RewardDistributorRobot::instantiate(
+        &runner,
+        &dependencies,
+        DEPS_PATH,
+        UNOPTIMIZED_PATH,
+        treasury_addr.address(),
+        &admin,
+        1000000u128,
+    );
+    let user = robot.reward_vault_robot.new_user(&admin);
 
     // Query LP token balance
-    let lp_token_balance = robot.query_cw20_balance(&robot.reward_lp_addr, &admin.address());
-    println!("LP token balance: {}", lp_token_balance);
+    let base_token_balance = robot
+        .reward_vault_robot
+        .query_base_token_balance(user.address());
+    println!("LP token balance: {}", base_token_balance);
 
     // Deposit to vault
     robot
-        .increase_cw20_allowance(
-            &robot.reward_lp_addr,
-            &robot.reward_vault_robot.vault_addr,
-            lp_token_balance,
-            &admin,
-        )
         .reward_vault_robot
-        .deposit_cw20_to_vault(lp_token_balance, &admin);
+        .deposit_cw20(base_token_balance, None, &user);
+
     // Query vault token balance
     let vault_token_balance = robot
         .reward_vault_robot
-        .query_vault_token_balance(admin.address());
-    assert_eq!(vault_token_balance, Uint128::from(lp_token_balance));
+        .query_vault_token_balance(user.address());
+
+    assert_eq!(vault_token_balance, base_token_balance);
 }
--- packages/reward-distributor/src/msg.rs
+++ packages/reward-distributor/src/msg.rs
@@ -1,8 +1,10 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{to_binary, CosmosMsg, Env, StdResult, Uint128, WasmMsg};
+use cw_dex::astroport::AstroportPool;
 use cw_ownable::{cw_ownable_execute, cw_ownable_query};
+use cw_vault_standard::VaultContract;
 
-use crate::ConfigUpdates;
+use crate::{Config, ConfigUpdates};
 
 #[cw_serde]
 pub struct InstantiateMsg {
@@ -53,7 +55,17 @@ pub enum ExecuteMsg {
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    #[returns(crate::Config)]
-    /// Returns the config of the contract
-    Config {},
+    #[returns(StateResponse)]
+    /// Returns the config of the contract as well as non-configurable contract
+    /// state
+    State {},
+}
+
+#[cw_serde]
+/// The response to a config query
+pub struct StateResponse {
+    pub config: Config,
+    pub reward_pool: AstroportPool,
+    pub reward_vault: VaultContract,
+    pub last_distributed: u64,
 }
